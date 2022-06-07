e93827e2a13def4136e5832ac38721355af6e493
Hardik Anand <41016443+Hardik981@users.noreply.github.com>
2022-06-07 21:41:38 +0530

    update Users.js

--- src/pages/Users.js
+++ src/pages/Users.js
@@ -1,4 +1,4 @@
-import { useState, useRef, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { Link } from "react-router-dom";
 import { useSelector, useDispatch } from 'react-redux';
 import { addUser, deleteUser } from '../redux/dataReducer';
